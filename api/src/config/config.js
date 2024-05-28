9711051b0e25d09ba0a377e546b38e6dd1f8f065
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-05-28 09:40:10 +0800

    Added sequelize config

---
+++ api/src/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: '../.env' });
+
+
+const dbConfig = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+};
+
+
+module.exports = () => ({
+  development: dbConfig,
+  test: dbConfig,
+  production: dbConfig,
+});
