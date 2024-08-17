979d5091213257d0c8d32d21f5a0c19b5cf5d1df
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-08-17 17:42:22 +0800

    created initial tags controller

---
+++ api/src/controllers/tags.controller.js
@@ -0,0 +1,77 @@
+const tagsService = require('../services')['tagsService'];
+
+exports.create = async (req, res) => {
+  try {
+    const postData = req.body;
+
+    const data = await tagsService.create(postData);
+
+    res.json({
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.findAll = async (req, res) => {
+  try {
+    const data = await tagsService.findAll();
+    res.json({
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.findOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await tagsService.findOne(id);
+
+    res.json({
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const putData = req.body;
+
+    const data = await tagsService.update(id, putData);
+
+    res.json({
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await tagsService.delete(id);
+
+    res.json({
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
