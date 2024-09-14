c8483ad8eee9b417da11e95a4b8ee00eade3a1fb
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-09-14 16:25:09 +0800

    fe: initial auth implem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,34 @@
-function App() {
-  return(
-    <div>Initial Bank Transactions Tracker FE</div>
-  )
-}
+// hooks
+import useAuthentication from './hooks/useAuthentication.hooks';
+
+// styles
+import 'firebaseui/dist/firebaseui.css';
+
+const App = () => {
+  const { authState, currentUser } = useAuthentication();
+
+  if (authState === 'loading') {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (authState === 'loggedIn') {
+    return (
+      <>
+        <h1>Welcome {currentUser?.displayName}</h1>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>Initial Bank Transactions Tracker FE</div>
+      <div id='firebaseui-auth-container'></div>
+    </>
+  );
+};
 
 export default App;
