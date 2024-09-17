fbf1f095db80e6483f2987b3e760ecf6a8e7b45e
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-09-17 22:07:50 +0800

    fe: Abstracted core logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,14 @@
-// hooks
-import useAuthentication from './hooks/useAuthentication.hooks';
+// core components
+import { AppRouter, AppAuthenticator } from './core';
 
 // styles
 import 'firebaseui/dist/firebaseui.css';
 
 const App = () => {
-  const { authState, currentUser } = useAuthentication();
-
-  if (authState === 'loading') {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
-  if (authState === 'loggedIn') {
-    return (
-      <>
-        <h1>Welcome {currentUser?.displayName}</h1>
-      </>
-    );
-  }
-
   return (
-    <>
-      <div>Initial Bank Transactions Tracker FE</div>
-      <div id='firebaseui-auth-container'></div>
-    </>
+    <AppAuthenticator>
+      <AppRouter />
+    </AppAuthenticator>
   );
 };
 
