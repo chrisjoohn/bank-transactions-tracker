491d8dcd6d13c76a89aec5f2999fd547daa40019
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-10-20 21:00:15 +0800

    fe: implemented font sizes in Pill component

--- client/src/components/atoms/Pill/Pill.tsx
+++ client/src/components/atoms/Pill/Pill.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 
 import './pill.styles.scss';
 
@@ -6,14 +7,15 @@ export type PillProps = {
   label: string;
   onClick?: () => void;
 
+  size?: '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
   active?: boolean;
 };
 
 const NavItem: FC<PillProps> = (props) => {
-  const { label, onClick, active } = props;
+  const { label, onClick, active, size = 'md' } = props;
   return (
     <div
-      className={`btt-pill ${active && 'active'}`}
+      className={classNames(`btt-pill`, active && 'active', `.font-${size}`)}
       onClick={onClick}
     >
       {label}
