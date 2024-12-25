7dd05223a010a6198bcf627bcc7081e16f1c65c7
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2024-12-25 20:33:43 +0800

    fix(auth): added auth headers on fe integrations

--- client/src/integration/apis/accounts/index.ts
+++ client/src/integration/apis/accounts/index.ts
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { getAuth } from 'firebase/auth';
+
 export type Account = {
   user_id: string;
   name: string;
@@ -9,7 +11,20 @@ export type Account = {
 
 export const accountsApi = createApi({
   reducerPath: 'accounts',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8080',
+    prepareHeaders: async (headers) => {
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      if (user) {
+        const token = await user.getIdToken();
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getAccounts: builder.query<Account[], void>({
       query: () => `/accounts`,
