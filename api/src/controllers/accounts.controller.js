e5eba9c6b2825d38de464492d9d9f5054052f56d
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-01-04 13:19:42 +0800

    chore(account-transactions): added endpoint for getting account transactions

--- api/src/controllers/accounts.controller.js
+++ api/src/controllers/accounts.controller.js
@@ -1,4 +1,6 @@
 const accountsService = require('../services')['accountsService'];
+const debitTransactionService = require('../services/debitTransactions.service');
+const creditTransactionService = require('../services/creditTransactions.service');
 
 exports.create = async (req, res) => {
   try {
@@ -34,6 +36,51 @@ exports.findAll = async (req, res) => {
 
     res.json({
       data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.findTransactions = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user_id = req.user.user_id;
+
+    const accountDetails = await accountsService.findOne(id, { user_id });
+
+    if (!accountDetails) {
+      res.status(400).json({
+        message: 'Bad request: Account not found!',
+      });
+      return;
+    }
+
+    let data;
+
+    if (accountDetails.type === 'CREDIT') {
+      data = await creditTransactionService.findAll({
+        account_id: accountDetails.id,
+      });
+    }
+
+    if (accountDetails.type === 'DEPOSIT') {
+      data = await debitTransactionService.findAll({
+        account_id: accountDetails.id,
+      });
+    }
+
+    if (data) {
+      res.json({
+        data,
+      });
+      return;
+    }
+
+    res.status(401).json({
+      message: 'Bad request',
     });
   } catch (err) {
     res.status(500).json({
