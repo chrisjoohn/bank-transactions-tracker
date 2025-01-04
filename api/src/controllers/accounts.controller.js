30bc0e99a9d024b49f64a0550b338b22d025744a
chrisjoohn <38459059+chrisjoohn@users.noreply.github.com>
2025-01-04 13:23:11 +0800

    chore(account-transactions-api): added API endpoint for getting account transactions (#11)
    * formatter(accounts-api): formatter updates
    
    * chore(account-transactions): added endpoint for getting account transactions
    
    * chore(accounts-api): pretty-format accounts controller

--- api/src/controllers/accounts.controller.js
+++ api/src/controllers/accounts.controller.js
@@ -1,4 +1,6 @@
-const accountsService = require('../services')['accountsService']
+const accountsService = require('../services')['accountsService'];
+const debitTransactionService = require('../services/debitTransactions.service');
+const creditTransactionService = require('../services/creditTransactions.service');
 
 exports.create = async (req, res) => {
   try {
@@ -42,6 +44,51 @@ exports.findAll = async (req, res) => {
   }
 };
 
+exports.findTransactions = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user_id = req.user.user_id;
+
+    const accountDetails = await accountsService.findOne(id, { user_id });
+
+    if (!accountDetails) {
+      res.status(400).json({
+        message: 'Bad request: Account not found!',
+      });
+      return;
+    }
+
+    let data;
+
+    if (accountDetails.type === 'CREDIT') {
+      data = await creditTransactionService.findAll({
+        account_id: accountDetails.id,
+      });
+    }
+
+    if (accountDetails.type === 'DEPOSIT') {
+      data = await debitTransactionService.findAll({
+        account_id: accountDetails.id,
+      });
+    }
+
+    if (data) {
+      res.json({
+        data,
+      });
+      return;
+    }
+
+    res.status(401).json({
+      message: 'Bad request',
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
 exports.findOne = async (req, res) => {
   try {
     const { id } = req.params;
@@ -69,8 +116,8 @@ exports.update = async (req, res) => {
     const toUpdate = await accountsService.findOne(id, { user_id });
     if (!toUpdate) {
       res.status(400).json({
-        message: `Can't find record to update: ${id}`
-      })
+        message: `Can't find record to update: ${id}`,
+      });
       return;
     }
 
@@ -94,8 +141,8 @@ exports.delete = async (req, res) => {
     const toDelete = await accountsService.findOne(id, { user_id });
     if (!toDelete) {
       res.status(400).json({
-        message: `Can't find record to update: ${id}`
-      })
+        message: `Can't find record to update: ${id}`,
+      });
       return;
     }
 
