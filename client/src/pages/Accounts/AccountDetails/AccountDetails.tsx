dc84ae92a4a20023320c7a421f6862741eaeb372
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-01-05 20:28:23 +0800

    chore(account-details): added initial integration on account details

--- client/src/pages/Accounts/AccountDetails/AccountDetails.tsx
+++ client/src/pages/Accounts/AccountDetails/AccountDetails.tsx
@@ -1,21 +1,32 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import classNames from 'classnames';
-
 import { useParams, useNavigate } from 'react-router-dom';
+
+import { Spin, Button } from 'antd';
+
+import { accountsApi } from '../../../integration/apis';
 
 const AccountDetails: FC = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!id) {
-      navigate('/');
-    }
-  }, [id]);
+  const { data, isFetching } = accountsApi.useGetAccountQuery(id || '');
+
+  if (isFetching) {
+    return <Spin size='large' />;
+  }
+
+  if (!data) {
+    return <h2>Error 404: Account Not Found</h2>;
+  }
+
+  const { name, description } = data;
 
   return (
     <div className={classNames('btt-account-details')}>
-      Account details page
+      <Button onClick={() => navigate(-1)}>Back</Button>
+      <h2>{name}</h2>
+      <p>{description}</p>
     </div>
   );
 };
