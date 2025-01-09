5ef3c38428ce3c6e2a7970197a8301d9e8147cb1
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-01-09 21:21:00 +0800

    chore(accounts-api): added transaction analytics endpoint

--- api/src/routes/accounts.routes.js
+++ api/src/routes/accounts.routes.js
@@ -8,6 +8,7 @@ module.exports = (app) => {
   const controller = require(`../controllers/accounts.controller`);
 
   router.get(`/${route}`, controller.findAll);
+  router.get(`/${route}/:id/transactions/analytics`, controller.transactionAnalytics);
   router.get(`/${route}/:id/transactions`, controller.findTransactions);
   router.get(`/${route}/:id`, controller.findOne);
   router.post(`/${route}`, controller.create);
