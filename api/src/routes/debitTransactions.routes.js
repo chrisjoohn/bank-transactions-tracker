9e79cf3b2a51c751d0a74f146ef77ca506f1a7e2
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-02-09 12:37:55 +0800

    chore(api-dbt-trx): updated statement parser endpoint

--- api/src/routes/debitTransactions.routes.js
+++ api/src/routes/debitTransactions.routes.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
   router.get(`/${route}/:id`, controller.findOne);
   router.post(`/${route}`, controller.create);
   router.post(`/${route}/bulk`, controller.bulkCreate);
-  router.post(`/${route}/statement-upload`, upload.single('file'), controller.parseStatement)
+  router.post(`/${route}/parse-statement`, upload.single('file'), controller.parseStatement);
   router.put(`/${route}/:id`, controller.update);
   router.delete(`/${route}/:id`, controller.delete);
 
