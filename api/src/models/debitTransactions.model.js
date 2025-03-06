9b53eb8ec0bf0c1ac4a76455966e8f50beedaebb
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-03-06 21:24:46 +0800

    chore(dbt-trx-api): updated debit transactions model

--- api/src/models/debitTransactions.model.js
+++ api/src/models/debitTransactions.model.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       };
       this.transaction_type = {
         type: DataTypes.ENUM,
-        values: ['DEPOSIT', 'CREDIT'],
+        values: ['INFLOW', 'OUTFLOW'],
         defaultValue: 'DEPOSIT',
       };
       this.amount = {
