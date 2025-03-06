742f0612cf25375a9d787dc11ff82ad5542c2605
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-03-06 22:50:37 +0800

    chore(trx-list): initial transaction list integration

--- client/src/pages/Accounts/AccountDetails/Transactions/TransactionList/TransactionList.tsx
+++ client/src/pages/Accounts/AccountDetails/Transactions/TransactionList/TransactionList.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
+import { format } from 'date-fns';
 
 // components
-import { Flex, Empty } from 'antd';
+import { Flex, Empty, Spin, Table } from 'antd';
+
+// apis
+import { debitTransactionsApi } from '../../../../../integration/apis';
 
 // type definitions
 import type { TransactionsProps } from '../Transactions';
+import type { DebitTransaction } from '../../../../../integration/apis/debit_transactions';
+
 export type TransactionListProps = {
   account: TransactionsProps['account'];
   filters?: {
@@ -14,13 +20,45 @@ export type TransactionListProps = {
 
 const TransactionList: FC<TransactionListProps> = (props) => {
   const {} = props;
+
+  const { isLoading, data } = debitTransactionsApi.useFindAllQuery();
+
   return (
     <Flex
       style={{ width: '100%' }}
       justify='center'
       align='center'
     >
-      <Empty />
+      {isLoading && <Spin size='large' />}
+      {data ? (
+        <Table<DebitTransaction>
+          dataSource={data}
+          scroll={{
+            y: 400,
+          }}
+          columns={[
+            {
+              title: 'Transaction Date',
+              dataIndex: 'transaction_date',
+            render: (item) => format(item, 'MMM dd, yyyy')
+            },
+            {
+              title: 'Description',
+              dataIndex: 'description',
+            },
+            {
+              title: 'Transaction Type',
+              dataIndex: 'transaction_type',
+            },
+            {
+              title: 'Amount',
+              dataIndex: 'amount',
+            },
+          ]}
+        />
+      ) : (
+        <Empty />
+      )}
     </Flex>
   );
 };
