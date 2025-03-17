6ac2dc5bc163002888ac4fef6caa5b1efce863e3
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-03-17 16:06:42 +0800

    chore: removed blocking of Credit Account details

--- client/src/pages/Accounts/AccountDetails/AccountDetails.tsx
+++ client/src/pages/Accounts/AccountDetails/AccountDetails.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import { useParams } from 'react-router-dom';
 import { endOfMonth, format, startOfMonth } from 'date-fns';
 
-import { Spin, Result } from 'antd';
+import { Spin } from 'antd';
 
 // major components
 import Analytics from './Analytics';
@@ -33,14 +33,6 @@ const AccountDetails: FC = () => {
     accountsApi.useLazyGetTransactionsQuery();
 
   useEffect(() => {
-    /**
-     * TO DO:
-     * This one's on hold cause components are not yet ready for CREDIT account transactions
-     */
-    if (accountDetails.data?.type === 'CREDIT') {
-      return;
-    }
-
     if (!accountDetails.data?.id) {
       return;
     }
@@ -66,16 +58,6 @@ const AccountDetails: FC = () => {
 
   if (!accountDetails.data) {
     return <h2>Error 404: Account Not Found</h2>;
-  }
-
-  if (accountDetails.data.type === 'CREDIT') {
-    return (
-      <Result
-        status='403'
-        title='Oops'
-        subTitle='This part is under construction'
-      />
-    );
   }
 
   const { name, description } = accountDetails.data;
