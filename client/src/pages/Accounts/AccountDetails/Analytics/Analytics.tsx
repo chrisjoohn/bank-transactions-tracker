dfaebe38c22d1110b292975411d8caa7c162688c
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-03-26 22:21:39 +0800

    chore: added render of account overview for credit accounts

--- client/src/pages/Accounts/AccountDetails/Analytics/Analytics.tsx
+++ client/src/pages/Accounts/AccountDetails/Analytics/Analytics.tsx
@@ -25,20 +25,11 @@ const Analytics: FC<AnalyticsProps> = (props) => {
       return;
     }
 
-    // blocked for now. to think of another concept for credi trx analytics
-    if (account.type === 'CREDIT') {
-      return;
-    }
-
     getAccountAnalytics({
       id: account.id,
       date_range: dateFilter,
     });
   }, [account.id, dateFilter]);
-
-  if (account.type === 'CREDIT') {
-    return null;
-  }
 
   if (accountAnalytics.isUninitialized || accountAnalytics.isLoading) {
     return <Spin size="large" />;
@@ -49,6 +40,23 @@ const Analytics: FC<AnalyticsProps> = (props) => {
   }
 
   const { totalInflow, totalOutflow, total } = accountAnalytics.data?.data;
+
+  // TODO: Update this one
+  if (account.type === 'CREDIT') {
+    return (
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card bordered={false}>
+            <Statistic
+              title="Outflow"
+              value={totalOutflow.toFixed(2)}
+              valueStyle={{ color: '#cf1322' }}
+            />
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
 
   return (
     <Row gutter={16}>
