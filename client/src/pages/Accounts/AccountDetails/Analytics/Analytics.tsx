80e0839c0b08d175928703419ca629e27c21731c
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-07-24 22:06:54 +0800

    chore: Added DynamicAnalytics component
    this component should be refactored/replanned

--- client/src/pages/Accounts/AccountDetails/Analytics/Analytics.tsx
+++ client/src/pages/Accounts/AccountDetails/Analytics/Analytics.tsx
@@ -1,4 +1,6 @@
 import { FC, useEffect } from 'react';
+import { LineChart, XAxis, YAxis, CartesianGrid, Legend, Line, Tooltip } from 'recharts';
+import DynamicAnalytics from '../DynamicAnalytics';
 
 import { Row, Col, Statistic, Card, Spin } from 'antd';
 
@@ -6,6 +8,7 @@ import { accountsApi } from '../../../../integration/apis';
 
 // type definitions
 import type { Account } from '../../../../integration/apis/accounts';
+import type { AnalyticsData } from '../DynamicAnalytics/DynamicAnalytics';
 
 export type AnalyticsProps = {
   account: Account;
@@ -14,6 +17,85 @@ export type AnalyticsProps = {
     end_date: string;
   };
 };
+
+const analyticsData: AnalyticsData = [
+  {
+    title: 'Necessary expenses',
+    chartType: 'line',
+    dimensions: ['week'],
+    metrics: [
+      {
+        key: 'food',
+        label: 'Food',
+      },
+      {
+        key: 'grocery',
+        label: 'Grocery',
+      },
+    ],
+    data: [
+      {
+        week: 'Week 1',
+        food: 600,
+        grocery: 2000,
+      },
+      {
+        week: 'Week 2',
+        food: 500,
+        grocery: 200,
+      },
+      {
+        week: 'Week 3',
+        food: 720,
+        grocery: 1800,
+      },
+      {
+        week: 'Week 4',
+        food: 100,
+        grocery: 0,
+      },
+    ],
+  },
+  {
+    title: 'Included/Not Included',
+    chartType: 'bar',
+    dimensions: ['week'],
+    metrics: [
+      {
+        key: 'included',
+        label: 'Included',
+        fill: '#8884d8',
+      },
+      {
+        key: 'not included',
+        label: 'Not Included',
+        fill: '#82ca9d',
+      },
+    ],
+    data: [
+      {
+        week: 'Week 1',
+        included: 600,
+        'not included': 2000,
+      },
+      {
+        week: 'Week 2',
+        included: 500,
+        'not included': 200,
+      },
+      {
+        week: 'Week 3',
+        included: 720,
+        'not included': 1800,
+      },
+      {
+        week: 'Week 4',
+        included: 100,
+        'not included': 0,
+      },
+    ],
+  },
+];
 
 const Analytics: FC<AnalyticsProps> = (props) => {
   const { account, dateFilter } = props;
@@ -54,6 +136,7 @@ const Analytics: FC<AnalyticsProps> = (props) => {
   if (account.type === 'CREDIT') {
     return (
       <Row gutter={16}>
+        <DynamicAnalytics data={analyticsData} />
         <Col span={8}>
           <Card bordered={false}>
             <Statistic
