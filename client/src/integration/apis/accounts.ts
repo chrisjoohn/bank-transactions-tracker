63182da847f84f431ac51cd653c653c329e6c25f
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-08-21 13:30:45 +0800

    chore: added tags filter on account transaction analytics

--- client/src/integration/apis/accounts.ts
+++ client/src/integration/apis/accounts.ts
@@ -8,6 +8,7 @@ import { transactionSliceActions } from '../slices/transactions.slice';
 import type { DebitTransaction, EditableDebitTransaction } from './debitTransactions';
 import type { CreditTransaction, EditableCreditTransaction } from './creditTransactions';
 import type { BaseEntityType, ParsedTrx } from '../types';
+import { Tag } from './tags';
 
 export interface Account extends BaseEntityType {
   user_id: string;
@@ -139,15 +140,17 @@ export const accountsApi = createApi({
         id?: Account['id'] | Account['unique_code'];
         post_date?: { start_date: string; end_date: string };
         transaction_date?: { start_date: string; end_date: string };
+        tags: Tag['id'][]
       }
     >({
-      query: ({ id, post_date, transaction_date }) => ({
+      query: ({ id, post_date, transaction_date, tags }) => ({
         method: 'POST',
         url: `/accounts/${id}/transactions/analytics`,
         body: {
           filters: {
             post_date,
             transaction_date,
+            tags,
           },
         },
       }),
