c27d4d0430c556d21de7946e6246b7c046d427ba
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-08-21 18:37:48 +0800

    chore(landing-page): dynamic features list

--- client/src/pages/Landing/sections/Features/Features.tsx
+++ client/src/pages/Landing/sections/Features/Features.tsx
@@ -2,31 +2,38 @@ import { FC } from 'react';
 
 import './features.styles.scss';
 
+const featureList = [
+  {
+    icon: '',
+    title: 'Multi-account tracking.',
+    description: 'Manage all your banks and cards in one view.',
+  },
+  {
+    icon: '',
+    title: 'Smart statement import.',
+    description: 'Simply drag, drop, and parse CSV or PDF.',
+  },
+  {
+    icon: '',
+    title: 'Cashflow insights.',
+    description: 'Visualize trends & spot opportunities.',
+  },
+];
+
 const Features: FC = () => {
   return (
     <section className="features">
-      <div className="item">
-        <div className="icon"></div>
-        <div className="details">
-          <div className="name">Multi-account tracking.</div>
-          <div className="description">Manage all your banks and cards in one view.</div>
-        </div>
-      </div>
-
-      <div className="item">
-        <div className="icon"></div>
-        <div className="details">
-          <div className="name">Multi-account tracking.</div>
-          <div className="description">Manage all your banks and cards in one view.</div>
-        </div>
-      </div>
-      <div className="item">
-        <div className="icon"></div>
-        <div className="details">
-          <div className="name">Multi-account tracking.</div>
-          <div className="description">Manage all your banks and cards in one view.</div>
-        </div>
-      </div>
+      {featureList.map((item) => {
+        return (
+          <div className="item">
+            <div className="icon"></div>
+            <div className="details">
+              <div className="name">{item.title}</div>
+              <div className="description">{item.description}</div>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
