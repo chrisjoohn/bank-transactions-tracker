d63ad7cc63549f7ec3bf1c5c0e9bf01c767482f3
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-08-21 22:04:24 +0800

    chore(login): Initial Login page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Accounts } from './Accounts';
 export { default as Tags } from './Tags';
 export { default as Transactions } from './Transactions';
 export { default as Landing } from './Landing';
+export { default as Login } from './Login';
