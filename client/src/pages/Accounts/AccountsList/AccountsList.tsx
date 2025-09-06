d14350078eb735ba4406ea755047a58db0f0db8d
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-09-06 18:30:40 +0800

    chore: implemented submit callback to navigate to new account after creation

--- client/src/pages/Accounts/AccountsList/AccountsList.tsx
+++ client/src/pages/Accounts/AccountsList/AccountsList.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // components
 import { Button, Modal } from 'antd';
@@ -10,6 +11,13 @@ import { useModal } from '../../../hooks';
 
 const AccountsList: FC = () => {
   const { showModal, toggleModal } = useModal();
+  const navigate = useNavigate();
+
+  const _submitCallback = (action: 'create' | 'update', accountId: string) => {
+    if (action === 'create') {
+      navigate(`/${accountId}`);
+    }
+  };
 
   return (
     <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
@@ -17,7 +25,7 @@ const AccountsList: FC = () => {
         Add Account
       </Button>
       <Modal open={showModal} footer={null} onCancel={() => toggleModal()}>
-        <AccountForm />
+        <AccountForm submitCallback={_submitCallback} />
       </Modal>
       <AccountList />
     </div>
