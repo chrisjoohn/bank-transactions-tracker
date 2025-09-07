7f40664ec9f023239a2e2be2b54063f5693621d6
chrisjoohn <38459059+chrisjoohn@users.noreply.github.com>
2025-09-07 14:28:56 +0800

    chore: initial creation of debit transaction tags (#47)
    * chore: created  debit-transaction-tags table
    
    * chore: initial setup for debit_transaction_tags
    
    * chore: fixed params for debit transaction tags
    
    * chore: removed unnecessary controller

---
+++ api/src/services/debitTransactionTags.service.js
@@ -0,0 +1,64 @@
+const models = require('../models');
+
+// required name to be used on exporting services on index
+exports.serviceName = 'debitTransactionTagService';
+
+exports.create = async ({ tag_id, debit_transaction_id }) => {
+  try {
+    const debitTransactionTagsModel = models.debit_transaction_tags;
+
+    const postData = {
+      tag_id,
+      debit_transaction_id,
+    };
+
+    const data = await debitTransactionTagsModel.create(postData);
+
+    return data;
+  } catch (err) {
+    console.log('Error in create debit_transaction_tags service: ', err);
+    throw err;
+  }
+};
+
+exports.findAll = async () => {
+  try {
+    const debitTransactionTagsModel = models.debit_transaction_tags;
+
+    const data = await debitTransactionTagsModel.findAll();
+    return data;
+  } catch (err) {
+    console.log('Error in find all debit_transaction_tags service: ', err);
+    throw err;
+  }
+};
+
+exports.findOne = async (id) => {
+  try {
+    const debitTransactionTagsModel = models.debit_transaction_tags;
+
+    const keyField = isNaN(id) ? 'unique_code' : 'id';
+
+    const data = await debitTransactionTagsModel.findOne({ where: { [keyField]: id } });
+
+    return data;
+  } catch (err) {
+    console.log('Error in find one debit_transaction_tags service: ', err);
+    throw err;
+  }
+};
+
+exports.delete = async (id) => {
+  try {
+    const debitTransactionTagsModel = models.debit_transaction_tags;
+
+    const keyField = isNaN(id) ? 'unique_code' : 'id';
+
+    const data = await debitTransactionTagsModel.destroy({ where: { [keyField]: id } });
+
+    return data;
+  } catch (err) {
+    console.log('Error in delete debit_transaction_tags service: ', err);
+    throw err;
+  }
+};
