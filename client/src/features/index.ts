f98d5d6a26122245628e08a50d780dbd81be4a7c
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2025-09-12 20:28:23 +0800

    chore: export TransactionDetails from features

--- client/src/features/index.ts
+++ client/src/features/index.ts
@@ -4,6 +4,7 @@
 export { default as Transactions } from './Transactions';
 export { default as TransactionList } from './TransactionList';
 export { default as TransactionStepperForm } from './TransactionStepperForm';
+export { default as TransactionDetails } from './TransactionDetails';
 
 /**
  * account-related features
