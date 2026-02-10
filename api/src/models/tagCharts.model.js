7bba1dc674c7754244d89803e1acb24688d020ad
Chris John Mulingbayan <chrisjohnmulingbayan@gmail.com>
2026-02-10 23:30:17 +0800

    chore(api): added tagCharts association

--- api/src/models/tagCharts.model.js
+++ api/src/models/tagCharts.model.js
@@ -38,7 +38,12 @@ module.exports = (sequelize, DataTypes) => {
 
   let Model = sequelize.define('bt_tag_charts', new TagCharts());
 
-  Model.associate = (models) => {};
+  Model.associate = (models) => {
+    Model.hasMany(models.account_tag_charts, {
+      sourceKey: 'id',
+      foreignKey: 'tag_chart_id'
+    });
+  };
 
   return Model;
 };
